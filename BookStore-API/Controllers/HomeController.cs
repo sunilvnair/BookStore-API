be05592cedfab0dbd180926cbea5454e4dc7eaf3
Sunil V Nair <sunilvnair@gmail.com>
2020-12-30 07:52:08 +0000

    First commit for book storeapi

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,79 @@
+﻿using BookStore_API.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+	/// <summary>
+	/// General Book Store API Controller
+	/// </summary>
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		private readonly ILoggerService _logger;
+
+        public HomeController(ILoggerService logger)
+        {
+			_logger = logger;
+
+		}	
+
+		/// <summary>
+		/// Get full details
+		/// </summary>
+		/// <returns></returns>
+		// GET: api/<HomeController>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			_logger.LogInfo(" This is an info msg from get method accessed from Home controller");
+			return new string[] { "value1", "value2" };
+		}
+
+		/// <summary>
+		/// Gets only details with parmeter
+		/// </summary>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		// GET api/<HomeController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			_logger.LogWarn(" This is an warning msg from get method with parm accessed from Home controller");
+
+			return "value";
+		}
+		/// <summary>
+		/// Methos for saving data using API
+		/// </summary>
+		/// <param name="value"></param>
+		// POST api/<HomeController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+			_logger.LogError(" This is an Error msg from post method  from Home controller");
+		}
+
+		// PUT api/<HomeController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+		/// <summary>
+		/// Delete methos using API with parameter
+		/// </summary>
+		/// <param name="id"></param>
+		// DELETE api/<HomeController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+			_logger.LogDebug(" This is a Debug msg from delete method with parm accessed from Home controller");
+		}
+	}
+}
---
+++ BookStore-API/Startup.cs
@@ -0,0 +1,106 @@
+using BookStore_API.Contracts;
+using BookStore_API.Data;
+using BookStore_API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace BookStore_API
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<ApplicationDbContext>(options =>
+				options.UseSqlServer(
+					Configuration.GetConnectionString("DefaultConnection")));
+			services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddEntityFrameworkStores<ApplicationDbContext>();
+			//services.AddRazorPages();
+
+			//Cors ==> shall allow us to access API from any other domain (access persmission granting)
+			services.AddCors(o=>{
+				o.AddPolicy("CrosPolicy",
+					builder => builder.AllowAnyOrigin()
+					.AllowAnyMethod()
+					.AllowAnyHeader());
+
+			});
+
+			services.AddSwaggerGen(c => {
+									c.SwaggerDoc("v1.0", new OpenApiInfo
+									{
+										Title = "Book Store API",
+										Version = "v1.0",
+										Description = "This is api documention for my first book store app in .net core "
+									});
+				var xfileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+				var xfilePath = Path.Combine(AppContext.BaseDirectory ,xfileName);
+				c.IncludeXmlComments(xfilePath);
+								});
+			//to include logging interface and service into project
+			services.AddSingleton<ILoggerService,LoggerService>();
+
+			//keep AddControllers as the last services ...
+			services.AddControllers();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseDatabaseErrorPage();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
+			app.UseSwagger();
+			app.UseSwaggerUI( c=> {
+				c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Book Store API");
+				c.RoutePrefix = "";
+			});
+
+
+			app.UseHttpsRedirection();
+			//	app.UseStaticFiles();
+
+			//cors policy name from ConfigureServices
+			app.UseCors("CrosPolicy");
+			app.UseRouting();
+
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
+}
